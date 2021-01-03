1ad9fe79c75d90866f85598b1e89d8caa163f0a8
Noah Bresler <noahbresler2002@gmail.com>
2021-01-03 17:21:55 -0600

    Remove unused import

--- FtcDashboard/dash/src/components/Tooltip.tsx
+++ FtcDashboard/dash/src/components/Tooltip.tsx
@@ -1,10 +1,4 @@
-import {
-  useEffect,
-  useRef,
-  FunctionComponent,
-  RefObject,
-  ReactNode,
-} from 'react';
+import { useRef, FunctionComponent, RefObject, ReactNode } from 'react';
 import { createPortal } from 'react-dom';
 
 import styled from 'styled-components';
